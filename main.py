1d1d8550d0df1190bc1c0e88fd0c77d4ddb5ca41
Matteo <mateus.sakata@gmail.com>
2021-02-05 01:01:47 -0600

    Add Pytest

---
+++ main.py
@@ -0,0 +1,8 @@
+import sys
+
+from autoupdater.countryscript.country_updater import Country_Updater
+
+
+# username, password = sys.argv[1:]
+# updater = Country_Updater(username, password)
+# updater.run()
