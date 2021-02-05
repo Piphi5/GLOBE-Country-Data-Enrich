af0359c9235dcb80fadab85423e2c78238ae9bf0
Matteo <mateus.sakata@gmail.com>
2021-02-05 01:15:24 -0600

    Update CI triggers

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ import sys
 from autoupdater.countryscript.country_updater import Country_Updater
 
 
-# username, password = sys.argv[1:]
-# updater = Country_Updater(username, password)
-# updater.run()
+username, password = sys.argv[1:]
+updater = Country_Updater(username, password)
+updater.run()
