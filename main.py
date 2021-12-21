88341cfe9a8261dc1c7e22f3501fb431eb7544b6
Matteo <mateus.sakata@gmail.com>
2021-12-21 10:34:37 -0600

    Fix mhm layer id

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from autoupdater.countryscript.country_updater import Country_Updater
 username, password = sys.argv[1:]
 mhm_updater = Country_Updater(
     "MosquitoHabitatMapper",
-    "2793f3d9c4d4447ea4f21b3ad74965d4",
+    "ae4990333e9f41f489550ddbe16a9d0f",
     "4e8bdb70b3d6424b8831e9cc621cf3b6",
     username,
     password,
